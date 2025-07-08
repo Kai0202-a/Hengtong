58eb443d15f462dd7dc7aebcb3b22f6879ad8571
Kai0202-a <a85709820@gmail.com>
2025-07-09 05:51:14 +0800

    修正貨況提醒框累加問題 - 調整 timeKey 生成邏輯以正確分組訂單

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -45,14 +45,14 @@ function Register() {
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 48 }}>
       <h2>申請帳號</h2>
       <form onSubmit={handleSubmit} style={{ width: 320, background: "#222", color: "#fff", padding: 24, borderRadius: 8 }}>
-        <input name="username" placeholder="帳號" value={form.username} onChange={handleChange} required style={{ width: "100%", marginBottom: 8 }} /><br/>
-        <input name="password" type="password" placeholder="密碼" value={form.password} onChange={handleChange} required style={{ width: "100%", marginBottom: 8 }} /><br/>
-        <input name="name" placeholder="姓名" value={form.name} onChange={handleChange} required style={{ width: "100%", marginBottom: 8 }} /><br/>
-        <input name="company" placeholder="公司名稱" value={form.company} onChange={handleChange} required style={{ width: "100%", marginBottom: 8 }} /><br/>
-        <input name="taxId" placeholder="公司統編" value={form.taxId} onChange={handleChange} required style={{ width: "100%", marginBottom: 8 }} /><br/>
-        <input name="address" placeholder="公司地址" value={form.address} onChange={handleChange} required style={{ width: "100%", marginBottom: 8 }} /><br/>
-        <input name="email" placeholder="Email" value={form.email} onChange={handleChange} style={{ width: "100%", marginBottom: 8 }} /><br/>
-        <input name="phone" placeholder="電話" value={form.phone} onChange={handleChange} style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="username" placeholder="帳號" value={form.username} onChange={handleChange} required autocomplete="username" style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="password" type="password" placeholder="密碼" value={form.password} onChange={handleChange} required autocomplete="new-password" style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="name" placeholder="姓名" value={form.name} onChange={handleChange} required autocomplete="name" style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="company" placeholder="公司名稱" value={form.company} onChange={handleChange} required autocomplete="organization" style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="taxId" placeholder="公司統編" value={form.taxId} onChange={handleChange} required autocomplete="off" style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="address" placeholder="公司地址" value={form.address} onChange={handleChange} required autocomplete="street-address" style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="email" placeholder="Email" value={form.email} onChange={handleChange} autocomplete="email" style={{ width: "100%", marginBottom: 8 }} /><br/>
+        <input name="phone" placeholder="電話" value={form.phone} onChange={handleChange} autocomplete="tel" style={{ width: "100%", marginBottom: 8 }} /><br/>
         <button type="submit" disabled={isSubmitting} style={{ width: "100%", opacity: isSubmitting ? 0.6 : 1 }}>
           {isSubmitting ? "提交中..." : "送出申請"}
         </button>
